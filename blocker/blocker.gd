8b51278e1e75dffc7d466bbc9583fab7b066f975
Nicholas S. Zivkovic <niki.zivkovic@hotmail.com>
2022-11-18 22:26:53 +0100

    adding bounce factor to blocker

--- blocker/blocker.gd
+++ blocker/blocker.gd
@@ -4,10 +4,13 @@ extends StaticBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+onready var bounce_factor
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if bounce_factor == null:
+			bounce_factor = .5
+	.set_bounce(bounce_factor)
 	pass # Replace with function body.
 
 
