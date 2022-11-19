ee7902f9470b6fa6f51af739cb0f60a95ef204d3
Shai <shai.halimi@gmail.com>
2022-11-19 11:19:10 +0100

    fixed sticky blockers

--- blocker/blocker.gd
+++ blocker/blocker.gd
@@ -7,15 +7,20 @@ extends StaticBody2D
 
 onready var bouncy_sprite = preload("res://blocker/bouncy_asset.png")
 onready var sticky_sprite = preload("res://blocker/sticky_asset.png")
+var blocker_type;
 
 
 func init(var type:int):
+
 	if (type == 0):
 		get_node("Sprite").texture = bouncy_sprite
+		blocker_type = 0;
 		.set_bounce(.5)
 	if (type == 1):
 		$Sprite.texture = sticky_sprite
 		.set_bounce(0)
+		blocker_type = 1;
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
