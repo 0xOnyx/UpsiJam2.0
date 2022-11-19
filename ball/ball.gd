6972ad769bc6d2e8384544d8c11ab582dbcfdfd4
Nicholas S. Zivkovic <niki.zivkovic@hotmail.com>
2022-11-19 02:09:11 +0100

    adding 4 types of balls with unique spries

--- ball/ball.gd
+++ ball/ball.gd
@@ -5,20 +5,52 @@ extends RigidBody2D
 # var a = 2
 # var b = "text"
 
-# also the mass which dictaes 
-export (int) var style
 
+var bounce_factor
+onready var style
+var number_of_styles = 4
 
 var out = 1;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	.set_bounce(.5)
-	pass # Replace with function body.
+	if style == null:
+		style = 1
+	if style == 0:
+		style1()
+	elif style == 1:
+		style1()
+	elif style == 2:
+		style2()
+	elif style == 3:
+		style3()
+		
+	if bounce_factor == null:
+		.set_bounce(.5)
+	.set_bounce(bounce_factor)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+func style0():
+	print("heavy ball")
+	$Sprite.frame = 1 
+	weight = 100 
+
+func style1():
+	print("low bounce ball")
+	$Sprite.frame = 2
+	bounce_factor = .1
+
+func style2():
+	print("medium bounce ball")
+	$Sprite.frame = 3
+	bounce_factor = .5
+
+func style3():
+	print("high bounce ball")
+	$Sprite.frame = 4
+	bounce_factor = .9
 
 func _on_VisibilityNotifier2D_screen_exited() -> void:
 	print("despawning ball")
