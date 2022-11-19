f8c65a34a484c53ed13c78735123baa4d8bef6f6
Shai <shai.halimi@gmail.com>
2022-11-19 04:55:03 +0100

    Added bounderies and changed moving system switching it to chunk moves

--- player/Cursor.gd
+++ player/Cursor.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var lifes: int = 10;
-var speed : int = 600;
+onready var speed : int = $CollisionShape2D.shape.extents.x;
 var vel = Vector2()
 
 export var player = "1";
@@ -21,6 +21,7 @@ onready var place = $block;
 var mirror = 1
 
 func _ready():
+	position = Vector2(0,0)
 	place.texture = sprite
 	place.modulate.a = .2
 	place.rotation = .45
@@ -28,20 +29,39 @@ func _ready():
 
 func _physics_process(delta):
 	modulate.a = 0.5
+	print(position)
+	print(get_viewport().size.y)
+	print(speed)
+	
 
 	vel.x = 0;
 	vel.y = 0;
 	if Input.is_action_just_pressed(inputs["mirror"]):
 		mirror *= -1
 		place.rotation *= -1
-	if Input.is_action_pressed(inputs["right"]):
-		 vel.x += speed;
-	if Input.is_action_pressed(inputs["left"]):
-		vel.x -= speed;
-	if Input.is_action_pressed(inputs["down"]):
-		vel.y += speed;
-	if Input.is_action_pressed(inputs["up"]):
-		vel.y -= speed;
+	if Input.is_action_just_pressed(inputs["right"]):
+		 position.x += speed;
+	if Input.is_action_just_pressed(inputs["left"]):
+		position.x -= speed;
+	if Input.is_action_just_pressed(inputs["down"]):
+		position.y += speed;
+	if Input.is_action_just_pressed(inputs["up"]):
+		position.y -= speed;
+		
+	
+	if player == "2" && position.x < get_viewport().size.x / 2 + speed:
+		position.x = get_viewport().size.x / 2 + speed;
+	if player == "1" && position.x > get_viewport().size.x / 2 - speed:
+		position.x = get_viewport().size.x / 2 - speed;
+	if position.x < speed:
+		position.x = speed;
+	if position.x > get_viewport().size.x - speed:
+		position.x = get_viewport().size.x - speed;
+	if position.y < speed:
+		position.y = speed;
+	if position.y > get_viewport().size.y - speed:
+		position.y = get_viewport().size.y - speed;
+		
 	if Input.is_action_just_pressed(inputs["place"]):
 		var block = flagPacked.instance();
 		block.position = position
@@ -50,4 +70,4 @@ func _physics_process(delta):
 		block.bounce_factor = 1
 		get_parent().add_child(block)
 
-	vel = move_and_slide(vel, Vector2.UP);
+	#vel = move_and_slide(vel, Vector2.UP);
