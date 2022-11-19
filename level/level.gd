edb9f2caf83c188c2c8e2777be164b7ce1d35004
onyx <info@jeremy.best>
2022-11-19 08:41:50 +0100

    merge

--- level/level.gd
+++ level/level.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 
+onready var score = [get_node("score1"), get_node("score2")]
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,7 +10,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	score[0].get_child(0).text = str(1)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- startscreen/StartScreen.gd
+++ startscreen/StartScreen.gd
@@ -5,20 +5,43 @@ extends Control
 # var a = 2
 # var b = "text"
 
+var current_select = 0
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
+onready var select_start = get_node("select_start")
+onready var select_exit = get_node("select_exit")
 
-func _on_StartButton_pressed():
-	get_tree().change_scene("res://level/level.tscn")
+func _process(delta):
+	if Input.is_action_just_pressed("1_move_down") or Input.is_action_just_pressed("1_move_up"):
+		current_select += 1
+		current_select %= 2
+		if (current_select == 0):
+			select_start.show()
+			select_exit.hide()
+		else :
+			select_start.hide()
+			select_exit.show()
+	if (Input.is_action_just_pressed("1_select") and current_select == 0):
+		get_tree().change_scene("res://level/level.tscn")
+	elif (Input.is_action_just_pressed("1_select") and current_select == 1):
+		get_tree().quit()
 
+func _on_Timer_timeout():
+	if (current_select == 0):
+		if (select_start.is_visible()):
+			select_start.hide()
+		else:
+			select_start.show()
+		select_exit.hide()
+	else:
+		if (select_exit.is_visible()):
+			select_exit.hide()
+		else:
+			select_exit.show()
+		select_start.hide()
 
-func _on_QuitButton_pressed():
-	get_tree().quit()
