17514a5999eb67fea62110342bc445fd64a53b09
onyx <info@jeremy.best>
2022-11-20 09:40:50 +0100

    feature add

--- TextStream/TextSteam.gd
+++ TextStream/TextSteam.gd
@@ -1,28 +1,32 @@
-extends Control
+extends Node
 
-var time = .9
+var timeAlive = 2
+var time = .1
 onready var timer = $Timer
 onready var lable = $Label
 var i = 1
+onready var wait = $wait
 
 func _ready():
 	init("./this is a long test")
-	pass # Replace with function body.
 
 func init(text_stream):
 	lable.set_text(text_stream)
-	time = time * lable.get_total_character_count()
-#	timer.set_wait_time(time)
-#	timer.start()
-	letter()
-
-func letter():
-	lable.set_visible_characters(i + 1)
 	timer.set_wait_time(time)
 	timer.start()
-	if lable.get_total_character_count() == i:
-		queue_free()
+
+func letter():
+	i += 1
+	lable.set_visible_characters(i)
+	if lable.get_total_character_count() <= i:
+		timer.stop()
+		wait.set_wait_time(timeAlive)
+		wait.start()
+
 
 func _on_Timer_timeout():
-	timer.stop()
 	letter()
+
+
+func _on_wait_timeout():
+	queue_free()
